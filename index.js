9aa7c9fd4bea9bf0228f63462be14d43b7dcaa9f
Jacob Baber <67894943+jacobbaber@users.noreply.github.com>
2022-07-17 22:11:29 -0500

    game actually works

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ function selectWord() {
 async function getDefinition(word) {
   const res = await axios.get(request + word);
   const definition = res.data[0]["meanings"][0]["definitions"][0]["definition"];
-  console.log(definition);
   showDefinition(definition);
 }
 
 function startGame() {
   const selectedWord = selectWord();
-  console.log(selectedWord);
   getDefinition(selectedWord);
   createLetterBoxes(selectedWord);
   letterBoxInputs();
+  revealLetter(selectedWord);
+  enterGuess(selectedWord);
 }
 
 function showDefinition(definition) {
@@ -40,19 +40,76 @@ function letterBoxInputs() {
   const letterBoxes = document.querySelectorAll(".letter-boxes");
   let place = 0;
   document.addEventListener("keyup", (e) => {
-    console.log(e);
-    if (e.keyCode >= "65" && e.keyCode <= "90" && place != letterBoxes.length) {
+    if (place < letterBoxes.length) {
+      while (
+        letterBoxes[place].classList.contains("hintedLetter") &&
+        place < letterBoxes.length - 1
+      ) {
+        ++place;
+      }
+    }
+    if (
+      e.keyCode >= "65" &&
+      e.keyCode <= "90" &&
+      place < letterBoxes.length &&
+      !letterBoxes[place].classList.contains("hintedLetter")
+    ) {
       letterBoxes[place].innerHTML = e.key.toUpperCase();
       if (place < letterBoxes.length) {
         ++place;
       }
-    } else if (e.key === "Backspace") {
-      if (place > 0) {
-        --place;
-      }
+    } else if (
+      e.key === "Backspace" &&
+      place > 0 &&
+      place <= letterBoxes.length &&
+      !letterBoxes[place - 1].classList.contains("hintedLetter")
+    ) {
+      --place;
       letterBoxes[place].innerHTML = "";
     }
   });
+}
+
+function revealLetter(selectedWord) {
+  let place = 0;
+  const letterBoxes = document.querySelectorAll(".letter-boxes");
+  const revealButton = document.querySelector(".reveal");
+  revealButton.addEventListener("click", () => {
+    if (place < selectedWord.length) {
+      letterBoxes[place].innerHTML = selectedWord[place].toUpperCase();
+      letterBoxes[place].classList.toggle("hintedLetter");
+      ++place;
+    }
+  });
+}
+
+function enterGuess(selectedWord) {
+  const enterGuessButton = document.querySelector(".enterGuess");
+  enterGuessButton.addEventListener("click", () => {
+    const guess = getGuess();
+    if (selectedWord == guess) {
+      gameCompleted();
+      console.log("You win!");
+    } else {
+      console.log("Wrong");
+    }
+  });
+}
+
+function getGuess() {
+  const letterBoxes = document.querySelectorAll(".letter-boxes");
+  let guess = "";
+  for (let letter of letterBoxes) {
+    guess = guess + letter.innerHTML;
+  }
+  return guess.toLowerCase();
+}
+
+function gameCompleted() {
+  const letterBoxes = document.querySelectorAll(".letter-boxes");
+  for (let letter of letterBoxes) {
+    letter.classList.add("guessedCorrectly");
+  }
 }
 
 startGame();
