f88f7ecdd22f0ca34aa824495a2ab87a9eac995d
Jacob Baber <67894943+jacobbaber@users.noreply.github.com>
2022-08-15 13:06:06 -0500

    changing webshare in attempt to fix bug

--- index.js
+++ index.js
@@ -360,8 +360,7 @@ function incorrectGuess() {
 const share = document.querySelector(".share");
 share.addEventListener("click", () => {
   navigator.share({
-    text: `I scored a ${getTotalScore()} in today's Definy`,
-    url: "https://jacobbaber.github.io/Definy/",
+    text: `I scored a ${getTotalScore()} in today's Definy. Play at https://jacobbaber.github.io/Definy/`,
   });
 });
 
